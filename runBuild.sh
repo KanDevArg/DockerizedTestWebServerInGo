387ff2f37e2c0969f3cb425540adb6b0aedbc067
Miguel S. Navarro <miguels.navarro@MacBook-Pro-de-Miguel.local>
2020-06-10 16:47:52 -0300

    Changes on shell file builder.

--- runBuild.sh
+++ runBuild.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 PRE_PWD=$(pwd)
+FOLDER_NAME=$(basename $(pwd))
 
 echo "Building main.go ..."
 
 mkdir -p bin
 
-go build -o $PRE_PWD/bin/app .
+go build -o $PRE_PWD/bin/$FOLDER_NAME .
 
 echo "Done."
